a859634068576b86f1a7738462d3e5a8b91ef149
root <vanniery.fernandes@outlook.com>
2019-03-22 13:16:47 -0300

    aula 22

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -18,6 +19,9 @@ class Category(models.Model):
 	def __str__(self):
 		return self.name
 
+	def get_absolute_url(self):
+		return reverse('catalog:category', kwargs={'slug':self.slug})
+
 class Product(models.Model):
 	
 	name = models.CharField('Nome', max_length=100)
@@ -36,3 +40,4 @@ class Product(models.Model):
 		ordering = ['name']
 	def __str__(self):
 		return self.name
+
--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,7 @@ app_name = 'catalog'
 urlpatterns = [
 	
 	url(r'^$',views.product_list, name='product_list'),
+	url(r'^(?P<slug>[\w_-]+)/$',views.category, name='category'),
+	  
 	
 ]
