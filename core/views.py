39d8a32d4f1931d9a0d022d341d99bb79785d5ae
root <vanniery.fernandes@outlook.com>
2019-03-28 17:37:48 -0300

    aula 30 terminar contato

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 from catalog.models import Category
+from .forms import ContactForm
 
 def index(request):
 	return render(request, 'index.html')
@@ -12,5 +13,15 @@ def product(request):
 def product_list(request):
 	return render(request, 'product_list.html')
 
+
 def contact(request):
-	return render(request, 'contact.html')
+  success = False
+  form = ContactForm(request.POST or None)
+  if form.is_valid():
+  	sucess = True
+  context = {
+   'form' : form,
+   'success':success
+  }
+
+  return render(request,'contact.html',context)
