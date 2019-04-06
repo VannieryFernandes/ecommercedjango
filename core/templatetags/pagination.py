5e316ae1e073535124ced94c57c5bcd6a425b299
root <vanniery.fernandes@outlook.com>
2019-04-05 23:00:44 -0300

    aula 36- falta mommy e whitenoise

--- core/templatetags/pagination.py
+++ core/templatetags/pagination.py
@@ -8,4 +8,12 @@ def pagination(request,paginator,page_obj):
     context['paginator']=paginator
     context['request']=request
     context['page_obj'] = page_obj
+    getvars = request.GET.copy()
+
+    if 'page' in getvars:
+        del getvars['page']
+    if len(getvars)>0:
+        context['getvars'] = '&{0}'.format(getvars.urlencode())
+    else:
+        context['getvars'] = ''
     return context
